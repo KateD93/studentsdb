67d005bfa3efe0134db2ec4adf493c8104a7ef67
Kate <Katewebwork@gmail.com>
2018-02-27 00:46:36 +0200

    add sorting by last_name, first name, and ticket_number, add arrow top and down

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.static import serve
 
 from students.views import students, groups
 
@@ -21,7 +22,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    # static files (images, css, javascript, etc.)
     urlpatterns += [
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
-        ]
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
